708a3741fb8c640796923ceb1035afe275f97bf1
andretecnologia <andre@andretecnologia.com.br>
2020-06-21 00:21:18 -0300

    delecao

--- clientes-app/src/app/app-routing.module.ts
+++ clientes-app/src/app/app-routing.module.ts
@@ -5,7 +5,8 @@ import { HomeComponent } from './home/home.component';
 
 
 const routes: Routes = [
-  { path : 'home', component: HomeComponent }
+  { path : 'home', component: HomeComponent },
+  { path : '', component: HomeComponent }
 ];
 
 @NgModule({
